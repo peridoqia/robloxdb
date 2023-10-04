dfe32f29e89b3fde7804bc889d363ff0f2d551e0
NodeMixaholic <9904667+NodeMixaholic@users.noreply.github.com>
2023-10-04 21:41:11 +0000

    fixes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express')
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 var app = express()
 let port = 24365
 
 
 app.get('/robloxdb/send-data', function (req, res) {
-    let placeID = req.params.id
-    let dataName = req.params.dataName
-    let data = req.params.data
+    let placeID = req.query.id.replace("?id=","").replace("&id=","")
+    let dataName = req.query.dataName.replace("?dataName=","").replace("&dataName=","")
+    let data = req.query.data.replace("?data=","").replace("&data=","")
     try {
         db.set(`${placeID}.${dataName}`, data);
         res.send(`OK! Set ${dataName} for ${placeID} to ${data} !`)
@@ -17,9 +19,9 @@ app.get('/robloxdb/send-data', function (req, res) {
 
 
 app.get('/robloxdb/push-data', function (req, res) {
-    let placeID = req.params.id
-    let dataName = req.params.dataName
-    let data = req.params.data
+    let placeID = req.query.id.replace("?id=","").replace("&id=","")
+    let dataName = req.query.dataName.replace("?dataName=","").replace("&dataName=","")
+    let data = req.query.data.replace("?data=","").replace("&data=","")
     try {
         db.push(`${placeID}.${dataName}`, data);
         res.send(`OK! Pushed 
@@ -31,8 +33,8 @@ app.get('/robloxdb/push-data', function (req, res) {
 })
 
 app.get('/robloxdb/get-data', function (req, res) {
-    let placeID = req.params.id
-    let dataName = req.params.dataName
+    let placeID = req.query.id.replace("?id=","").replace("&id=","")
+    let dataName = req.query.dataName.replace("?dataName=","").replace("&dataName=","")
     try {
         let data = db.get(`${placeID}.${dataName}`);
         res.send(`${data}`)
